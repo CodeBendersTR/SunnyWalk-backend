4ff8bc6fc60f155403173483d30d9aebb2f6080b
Marius Grigore <mariusgrygore@gmail.com>
2021-04-05 15:02:34 +0100

    Created model and repository for users_sessions table (forgot)

---
+++ src/main/java/com/codebenders/sunnywalkbackend/model/UserSession.java
@@ -0,0 +1,9 @@
+package com.codebenders.sunnywalkbackend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users_sessions")
+public class UserSession {
+}
