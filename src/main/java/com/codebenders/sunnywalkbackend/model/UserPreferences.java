22612ca5e8e502291dc7045e2088447a2af5a343
Marius Grigore <mariusgrygore@gmail.com>
2021-04-06 00:27:51 +0100

    Implemented UserPreferences model

--- src/main/java/com/codebenders/sunnywalkbackend/model/UserPreferences.java
+++ src/main/java/com/codebenders/sunnywalkbackend/model/UserPreferences.java
@@ -5,4 +5,59 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users_preferences")
 public class UserPreferences {
+    @Id
+    @Column(name = "user_id")
+    Integer userId;
+
+    @Column(name = "weather")
+    String weather;
+
+    @Column(name = "push_notifications")
+    Boolean pushNotifications;
+
+    @Column(name = "mail_notifications")
+    Boolean mailNotifications;
+
+    @Column(name = "cookies")
+    Boolean cookies;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public Boolean getPushNotifications() {
+        return pushNotifications;
+    }
+
+    public void setPushNotifications(Boolean pushNotifications) {
+        this.pushNotifications = pushNotifications;
+    }
+
+    public Boolean getMailNotifications() {
+        return mailNotifications;
+    }
+
+    public void setMailNotifications(Boolean mailNotifications) {
+        this.mailNotifications = mailNotifications;
+    }
+
+    public Boolean getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(Boolean cookies) {
+        this.cookies = cookies;
+    }
 }
