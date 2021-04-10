fdfadd58347cd54ebcb263db13e5407418b58b2b
Marius Grigore <mariusgrygore@gmail.com>
2021-04-10 20:00:35 +0100

    Implemented UserSesion model

--- src/main/java/com/codebenders/sunnywalkbackend/model/UserSession.java
+++ src/main/java/com/codebenders/sunnywalkbackend/model/UserSession.java
@@ -1,9 +1,67 @@
 package com.codebenders.sunnywalkbackend.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "user_session")
 public class UserSession {
+    @Id
+    @Column(name = "session_id")
+    String sessionId;
+
+    @Column(name = "user_id")
+    int userId;
+
+    @Column(name = "opened")
+    Date opened;
+
+    @Column(name = "active")
+    Boolean active;
+
+    @Column(name = "ip_address")
+    String ipAddress;
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getOpened() {
+        return opened;
+    }
+
+    public void setOpened(Date opened) {
+        this.opened = opened;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 }
