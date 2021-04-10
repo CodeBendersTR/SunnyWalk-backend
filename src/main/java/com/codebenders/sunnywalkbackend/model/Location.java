dde140d435fe8d41de56af16fe14a8f7ba8a3e34
Marius Grigore <mariusgrygore@gmail.com>
2021-04-10 19:59:24 +0100

    Implemented Location model

--- src/main/java/com/codebenders/sunnywalkbackend/model/Location.java
+++ src/main/java/com/codebenders/sunnywalkbackend/model/Location.java
@@ -5,5 +5,59 @@ import javax.persistence.*;
 @Entity
 @Table(name = "location")
 public class Location {
+    @Id
+    @Column(name = "location_id")
+    int locationId;
 
+    @Column(name = "location_name")
+    String locationName;
+
+    @Column(name = "latitude")
+    String latitude;
+
+    @Column(name = "longitude")
+    String longitude;
+
+    @Column(name = "check")
+    Boolean check;
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public Boolean getCheck() {
+        return check;
+    }
+
+    public void setCheck(Boolean check) {
+        this.check = check;
+    }
 }
