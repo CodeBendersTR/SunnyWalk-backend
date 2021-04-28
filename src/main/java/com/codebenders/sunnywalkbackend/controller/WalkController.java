869f38eb3a0a3a7bb61659da2a875bb92c36a1b1
Marius Grigore <mariusgrygore@gmail.com>
2021-04-28 22:17:22 +0100

    Extended session functionality to all controllers

--- src/main/java/com/codebenders/sunnywalkbackend/controller/WalkController.java
+++ src/main/java/com/codebenders/sunnywalkbackend/controller/WalkController.java
@@ -2,6 +2,10 @@ package com.codebenders.sunnywalkbackend.controller;
 
 import com.codebenders.sunnywalkbackend.dto.AddWalkDto;
 import com.codebenders.sunnywalkbackend.dto.WalkSuggestionDto;
+import com.codebenders.sunnywalkbackend.model.User;
+import com.codebenders.sunnywalkbackend.repository.UserRepository;
+import com.codebenders.sunnywalkbackend.repository.UserSessionRepository;
+import com.codebenders.sunnywalkbackend.service.IAuthService;
 import com.codebenders.sunnywalkbackend.service.IWalkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,13 +21,27 @@ public class WalkController {
     @Autowired
     IWalkService walkService;
 
-    @GetMapping("/suggestion/{userId}")
-    public ArrayList<WalkSuggestionDto> getWalkSuggestion(@PathVariable int userId) {
-        return walkService.getSuggestion(userId);
+    @Autowired
+    IAuthService authService;
+
+    @Autowired
+    UserSessionRepository userSessionRepository;
+
+    @GetMapping("/suggestion")
+    public ResponseEntity<ArrayList<WalkSuggestionDto>> getWalkSuggestion(@RequestParam(required = false) String sessionId) {
+        if (sessionId == null || !authService.isUserLoggedIn(sessionId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+        int userId = userSessionRepository.getOne(sessionId).getUserId();
+        return ResponseEntity.status(HttpStatus.OK).body(walkService.getSuggestion(userId));
     }
 
-    @PostMapping("/add/{userId}")
-    public ResponseEntity<String> addWalk(@RequestBody AddWalkDto addWalkDto, @PathVariable int userId) {
+    @PostMapping("/add")
+    public ResponseEntity<String> addWalk(@RequestBody AddWalkDto addWalkDto, @RequestParam(required = false) String sessionId) {
+        if (sessionId == null || !authService.isUserLoggedIn(sessionId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not logged in");
+        }
+        int userId = userSessionRepository.getOne(sessionId).getUserId();
         walkService.addWalk(userId, addWalkDto);
         return new ResponseEntity<String>("Walk added successfully", HttpStatus.OK);
     }
